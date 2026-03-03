d689ae9dea12bd867ed155f73c1786dd86299e66
FridrichMethod <zhaoyangli@stanford.edu>
2026-03-03 11:20:48 -0800

    add predict.sh script for model inference with Protenix

---
+++ predict.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2024 ByteDance and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+export PROTENIX_ROOT_DIR="/apps/Protenix"
+
+protenix pred \
+    -i "$1" \
+    -o "$2" \
+    --seeds 101 \
+    --model_name protenix_base_20250630_v1.0.0 \
+    --use_msa true \
+    --use_template true \
+    --use_default_params false \
+    --sample 5 \
+    --step 200 \
+    --cycle 10
